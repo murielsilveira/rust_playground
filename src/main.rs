b12c8c661b030cb31a88aa4ca674f82dbdafcabd
Muriel Silveira <murielkong@gmail.com>
2023-06-20 15:30:58 +0200

    feat: wip? playing around with something

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
 fn main() {
-    println!("Hello, world!");
+    let provider = get_provider("internal").unwrap();
+    import_stuff(&provider);
+}
+
+trait Provider {
+    fn do_something(&self) -> String;
+}
+
+struct ExternalProvider;
+
+struct InternalProvider;
+
+impl Provider for ExternalProvider {
+    fn do_something(&self) -> String {
+        println!("External");
+        String::from("External")
+    }
+}
+
+impl Provider for InternalProvider {
+    fn do_something(&self) -> String {
+        println!("Internal");
+        String::from("Internal")
+    }
+}
+
+fn get_provider(provider_key: &str) -> Result<Box<dyn Provider>, String> {
+    match provider_key {
+        "external" => Ok(Box::new(ExternalProvider)),
+        "internal" => Ok(Box::new(InternalProvider)),
+        _ => Err(format!("Invalid provider [{provider_key}].")),
+    }
+}
+
+fn import_stuff(provider: &Box<dyn Provider>) -> String {
+    provider.do_something()
 }
