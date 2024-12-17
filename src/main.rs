89535b9a3ac8f4cf4956ffd9adf41205ae3b8e90
Muriel Silveira <murielkong@gmail.com>
2024-12-17 16:52:26 +0100

    chore: chrono timezone conversions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use chrono::{DateTime, NaiveDate, TimeZone, Utc};
+use chrono_tz::Tz;
 use rust_decimal::Decimal;
 use rust_xlsxwriter::Workbook;
 
@@ -18,6 +20,17 @@ fn main() {
     println!("{} {}", z, z.abs() < 0.001);
 
     println!("default status {:?}", Status::default());
+
+    // Utc date from local date and back to naive
+    let tz: Tz = String::from("Europe/London").parse().unwrap();
+    let date = NaiveDate::from_ymd_opt(2024, 05, 2).unwrap();
+    let utc_date: DateTime<Utc> = tz
+        .from_local_datetime(&date.and_hms_opt(0, 0, 0).unwrap())
+        .unwrap()
+        .with_timezone(&Utc);
+    let local_naive = tz.from_utc_datetime(&utc_date.naive_utc()).date_naive();
+
+    println!("{} {}", utc_date, local_naive);
 }
 
 #[derive(Debug)]
